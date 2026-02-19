2d087dc5cf163ea3497eb52b35d7b63bc31e1cbc
xplicitDev <masoodforwork@gmail.com>
2026-02-19 21:46:58 +0530

    .

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+const JWT_secret = "masood";
+
+module.exports = JWT_secret;
--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,10 @@ const mainRouter = require("./routes/index");
 const cors = require("cors");
 
 app.use(cors());
+app.use(express.json());
 
 const app = express();
 
 app.use("/api/v1", mainRouter);
+
+app.listen(3000);
