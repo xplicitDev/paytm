d41c4061802e5ebfc11f011cabeea12df3e30571
xplicitDev <masoodforwork@gmail.com>
2026-02-21 22:10:39 +0530

    .

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,48 @@
 const express = require("express");
+const zod = require("zod");
+const jwt = require("jsonwebtoken");
+const { User } = require("../db");
+const { JWT_SECRET } = require("../config");
 
-const router = express.router();
+const router = express.Router();
+
+const signupSchema = zod.object({
+  username: zod.string(),
+  firstName: zod.string(),
+  lastName: zod.string(),
+  password: zod.string(),
+});
+
+router.post("/signup", async (req, res) => {
+  const body = req.body;
+  const { success } = signupSchema.safeParse(req.body);
+  if (!success) {
+    return res.json({
+      message: "Incorrect Inputs / Email already taken",
+    });
+  }
+
+  const user = User.findOne({
+    username: body.username,
+  });
+
+  if (user._id) {
+    return res.json({
+      message: "Incorrect Inputs / Email already taken",
+    });
+  }
+
+  const dbUser = await User.create(body);
+  const token = jwt.sign(
+    {
+      userId: dbUser._id,
+    },
+    JWT_SECRET,
+  );
+  res.json({
+    message: "User created Successfully.",
+    token: token,
+  });
+});
 
 module.exports = router;
